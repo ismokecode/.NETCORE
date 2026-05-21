355a9ddff928a9aa48329fa9001f068828dbb8f2
ismokecode <rajeevkr.co.in@gmail.com>
2026-05-21 09:37:35 +0530

    random ques for visitors

--- SWSS_v1/UnitOfWork/QuestionRepository.cs
+++ SWSS_v1/UnitOfWork/QuestionRepository.cs
@@ -91,7 +91,10 @@ namespace SWSS_v1.UnitOfWork
         }
         public async Task<IQueryable<Question>> GetQuizQuestionByClassAndSubject(int clsId, int subId, int instituteId)
         {
+            int count = await _context.Questions.CountAsync();
+            int index = new Random().Next(0, count);
             var questionWithOptions = _context.Questions
+                             .Skip(index)
                              .Where(x => x.InstituteId == instituteId && x.ClassID == clsId && x.SubjectId == subId && x.isSetAsQuestion==true)
                              .Include(p => p.Options)
                              .Include(p => p.Classes)
